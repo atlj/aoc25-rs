b1e1643a2af2000be2aee3f127fa996a7ad61a57
Burak Güner <burak.guner@callstack.com>
2025-12-03 12:57:29 +0300

    day 3 part 2

--- src/bin/3/main.rs
+++ src/bin/3/main.rs
@@ -1,4 +1,4 @@
-const INPUT: &str = include_str!("./test.txt");
+const INPUT: &str = include_str!("./input.txt");
 
 fn main() {
     let result: usize = INPUT
@@ -6,7 +6,7 @@ fn main() {
         .map(parse_bank)
         .map(|iter| iter.collect::<Box<[_]>>())
         // .inspect(|bank| {
-        //     dbg!(bank.clone().collect::<Vec<_>>());
+        //     dbg!(&bank);
         // })
         .map(|bank| find_max_possible_joltage(&bank, 12))
         .sum();
@@ -21,25 +21,30 @@ fn parse_bank(input: &str) -> impl Iterator<Item = u8> + Clone {
 fn find_max_possible_joltage(bank: &[u8], count: u8) -> usize {
     let len = bank.len();
 
-    if count as usize > len {
+    if usize::from(count) > len {
         panic!()
     }
 
     (0..count)
         .rev()
         .fold((0_usize, 0), |(start_index, acc), pow| {
-            let end_index = len - pow as usize - 1;
+            let end_index = len - usize::from(pow) - 1;
             let (index, max_digit) = bank
                 .iter()
                 .enumerate()
                 .skip(start_index)
                 .take_while(|(index, _)| index <= &end_index)
-                .max_by_key(|item| item.1)
+                .max_by(
+                    |(index_a, digit_a), (index_b, digit_b)| match digit_a.cmp(digit_b) {
+                        std::cmp::Ordering::Equal => index_b.cmp(index_a),
+                        _ => digit_a.cmp(digit_b),
+                    },
+                )
                 .unwrap();
 
             (
                 index + 1,
-                acc + (*max_digit as usize) * 10_usize.pow(pow as u32),
+                acc + (usize::from(*max_digit)) * 10_usize.pow(u32::from(pow)),
             )
         })
         .1

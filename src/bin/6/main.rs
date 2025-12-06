f7a992c7ce3bdae5b9c39cea01769e19f4126151
Burak Güner <burak.guner@callstack.com>
2025-12-06 14:15:19 +0300

    day 6 part 2

--- src/bin/6/main.rs
+++ src/bin/6/main.rs
@@ -3,15 +3,48 @@ use std::str::Chars;
 const INPUT: &str = include_str!("./input.txt");
 
 fn main() {
+    part1();
+    part2();
+}
+
+fn part2() {
     let mut lines = INPUT.lines().rev();
     let ops_row = lines.next().unwrap();
-    let ops = parse_ops_row(&ops_row);
+    let ops = parse_ops_row(ops_row);
 
-    let mut nums_rows_iterators = lines
-        .map(|nums_row| parse_nums_row(nums_row))
-        .collect::<Vec<_>>();
+    let mut char_iters: Vec<_> = lines.rev().map(|row| row.chars()).collect();
+    let mut num_cols = NumCols {
+        char_iters: &mut char_iters,
+    };
 
-    let result: usize = ops
+    let part2_result: usize = ops
+        .map(|operation| {
+            let init = match operation {
+                Operation::Add => 0,
+                Operation::Multiply => 1,
+            };
+
+            num_cols
+                .by_ref()
+                .take_while(|num| num != &0)
+                .fold(init, |acc, num| match operation {
+                    Operation::Add => acc + num,
+                    Operation::Multiply => acc * num,
+                })
+        })
+        .sum();
+
+    dbg!(part2_result);
+}
+
+fn part1() {
+    let mut lines = INPUT.lines().rev();
+    let ops_row = lines.next().unwrap();
+    let ops = parse_ops_row(ops_row);
+
+    let mut nums_rows_iterators = lines.map(parse_nums_row).collect::<Vec<_>>();
+
+    let part1_result: usize = ops
         .map(|operation| {
             let mut result = match operation {
                 Operation::Add => 0,
@@ -29,7 +62,34 @@ fn main() {
         })
         .sum();
 
-    dbg!(result);
+    dbg!(part1_result);
+}
+
+struct NumCols<'a, 'b>
+where
+    'b: 'a,
+{
+    char_iters: &'a mut [Chars<'b>],
+}
+
+impl Iterator for NumCols<'_, '_> {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut result = 0;
+
+        for char_iter in self.char_iters.iter_mut() {
+            let char = char_iter.next()?;
+            if let Some(digit) = char
+                .to_digit(10)
+                .map(|digit| usize::try_from(digit).unwrap())
+            {
+                result = result * 10 + digit
+            }
+        }
+
+        Some(result)
+    }
 }
 
 fn parse_nums_row(row: &str) -> impl Iterator<Item = usize> {
